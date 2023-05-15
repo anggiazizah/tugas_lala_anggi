5cc30affb5245f7ab741dcaef0262d63e3c29738
anggiazizah <anggiazizah18@gmail.com>
2023-05-16 01:40:32 +0700

    membuat index php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Form Biodata Kelas</title>
+</head>
+<body>
+   <div class="wrap">
+    <div class="container">
+    <form action="" method= "">
+    <h1>Form Biodata Kelas</h1>
+    <table border="0">
+        <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td><input type="text"></td>
+        </tr>
+        <tr>
+            <td>Kelas</td>
+            <td>:</td>
+            <td><select>
+                <option>11 RPL 1</option>
+                <option>11 RPL 2</option>
+            </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>:</td>
+            <td><textarea cols="30" rows="10"></textarea></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td>
+                <button type="submit" name="kirim">Kirim</button>
+                <button type="reset" name="reset">Batal</button>
+            </td>
+        </tr>
+    </table>
+   </Form>
+   </div>
+    </div>
+</body>
+</html>
